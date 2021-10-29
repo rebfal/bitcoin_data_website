6cbdcf6548d7313567d977506d5dcc7695f87e07
Rebecca Fällström <rebecca@MacBook-Air-som-tillhor-Rebecca.local>
2021-10-29 13:31:20 +0200

    new repo

---
+++ server.py
@@ -0,0 +1,54 @@
+from sqlite3 import Connection as SQLite3Connection
+from datetime import datetime
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+#import linked_list
+#import hash_table
+#import binary_search_tree
+#import custom_q
+#import stack
+import random
+
+# app
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
+
+
+
+@app.route('/')
+
+# configure sqlite3 to enforce foreign key contraints
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
+
+
+db = SQLAlchemy(app)
+now = datetime.now()
+
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    address = db.Column(db.String(200))
+    phone = db.Column(db.String(50))
+    posts = db.relationship("BlogPost", cascade="all, delete")
+
+
+# like what has been done for User,
+# Question 2: create a Blogpost database model    
+class BlogPost(db.Model):
+   
+
+    if __name__ == "__main__":
+        db.create_all()
+        #app.run(host='localhost', port=3000)
+   # app.run(debug=True)
